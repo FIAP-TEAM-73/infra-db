a94545bc037cd56899bebf07f2e782ab8e2a05eb
acdmAnderson <acdm.anderson@hotmail.com>
2024-09-25 00:29:20 -0300

    fix: terraform format

--- secret.tf
+++ secret.tf
@@ -1,5 +1,5 @@
 resource "aws_secretsmanager_secret" "db_password" {
-  name = "db_password"
+  name                    = "db_password"
   recovery_window_in_days = 0
 }
 
@@ -9,7 +9,7 @@ resource "aws_secretsmanager_secret_version" "db_password" {
 }
 
 resource "aws_secretsmanager_secret" "db_user" {
-  name = "db_user"
+  name                    = "db_user"
   recovery_window_in_days = 0
 }
 
@@ -19,24 +19,24 @@ resource "aws_secretsmanager_secret_version" "db_user" {
 }
 
 resource "aws_secretsmanager_secret" "db_url" {
-  name = "db_url"
+  name                    = "db_url"
   recovery_window_in_days = 0
 }
 
 resource "aws_secretsmanager_secret_version" "db_url" {
   secret_id     = aws_secretsmanager_secret.db_url.id
   secret_string = aws_db_instance.postgres-db-instance.address
-  depends_on = [ aws_db_instance.postgres-db-instance ]
-  
+  depends_on    = [aws_db_instance.postgres-db-instance]
+
 }
 
 resource "aws_secretsmanager_secret" "db_host" {
-  name = "db_host"
+  name                    = "db_host"
   recovery_window_in_days = 0
 }
 
 resource "aws_secretsmanager_secret_version" "db_host" {
   secret_id     = aws_secretsmanager_secret.db_host.id
   secret_string = "postgres://${aws_secretsmanager_secret_version.db_user.secret_string}:${aws_secretsmanager_secret_version.db_password.secret_string}@${aws_secretsmanager_secret_version.db_url.secret_string}:5432/database_fastfood"
-  depends_on = [ aws_db_instance.postgres-db-instance, aws_secretsmanager_secret.db_url, aws_secretsmanager_secret.db_password, aws_secretsmanager_secret.db_user ]
+  depends_on    = [aws_db_instance.postgres-db-instance, aws_secretsmanager_secret.db_url, aws_secretsmanager_secret.db_password, aws_secretsmanager_secret.db_user]
 }
