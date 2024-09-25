409b3314ddeecf2129eb84ee79d325ab02298b98
acdmAnderson <acdm.anderson@hotmail.com>
2024-09-25 00:26:49 -0300

    chore: adding extra secrets to terraform

--- secret.tf
+++ secret.tf
@@ -1,5 +1,6 @@
 resource "aws_secretsmanager_secret" "db_password" {
   name = "db_password"
+  recovery_window_in_days = 0
 }
 
 resource "aws_secretsmanager_secret_version" "db_password" {
@@ -9,9 +10,33 @@ resource "aws_secretsmanager_secret_version" "db_password" {
 
 resource "aws_secretsmanager_secret" "db_user" {
   name = "db_user"
+  recovery_window_in_days = 0
 }
 
 resource "aws_secretsmanager_secret_version" "db_user" {
   secret_id     = aws_secretsmanager_secret.db_user.id
   secret_string = var.rds_postgre_user
+}
+
+resource "aws_secretsmanager_secret" "db_url" {
+  name = "db_url"
+  recovery_window_in_days = 0
+}
+
+resource "aws_secretsmanager_secret_version" "db_url" {
+  secret_id     = aws_secretsmanager_secret.db_url.id
+  secret_string = aws_db_instance.postgres-db-instance.address
+  depends_on = [ aws_db_instance.postgres-db-instance ]
+  
+}
+
+resource "aws_secretsmanager_secret" "db_host" {
+  name = "db_host"
+  recovery_window_in_days = 0
+}
+
+resource "aws_secretsmanager_secret_version" "db_host" {
+  secret_id     = aws_secretsmanager_secret.db_host.id
+  secret_string = "postgres://${aws_secretsmanager_secret_version.db_user.secret_string}:${aws_secretsmanager_secret_version.db_password.secret_string}@${aws_secretsmanager_secret_version.db_url.secret_string}:5432/database_fastfood"
+  depends_on = [ aws_db_instance.postgres-db-instance, aws_secretsmanager_secret.db_url, aws_secretsmanager_secret.db_password, aws_secretsmanager_secret.db_user ]
 }
